1f770a4df476793523900466369933e109df410f
Kamil Pietruszka <k.pietruszka@avsystem.com>
2019-01-01 22:51:03 +0100

    Tables are being created on init.

--- src/main/scala/db/Db.scala
+++ src/main/scala/db/Db.scala
@@ -1,10 +1,13 @@
 package db
 
 import com.typesafe.config.ConfigFactory
+import dao.HasTableQuery
 import slick.basic.DatabaseConfig
 import slick.jdbc.H2Profile
+import slick.jdbc.H2Profile.api._
 
-final class Db(configPath: String) {
+
+final class Db(configPath: String) extends HasTableQuery {
   val instance = {
     val config = ConfigFactory.load()
     val databaseConfig: DatabaseConfig[H2Profile.type] = DatabaseConfig.forConfig[H2Profile.type](configPath, config)
@@ -12,5 +15,6 @@ final class Db(configPath: String) {
   }
   locally {
     instance.createSession()
+    instance.run(DBIO.seq((accounts.schema ++ transactions.schema).create))
   }
 }

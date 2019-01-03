609a434d56a9dcd870bdfda711aed3c77646a936
kamilos147@gmail.com <Crossy147>
2019-01-03 16:31:06 +0100

    Removed Fatal exception case handling from partial-function handler

--- src/main/scala/api/ApiExceptionHandler.scala
+++ src/main/scala/api/ApiExceptionHandler.scala
@@ -6,12 +6,14 @@ import akka.http.scaladsl.server.{Directives, ExceptionHandler}
 import com.typesafe.scalalogging.LazyLogging
 import exceptions.IllegalRestCallArgumentsException
 
+import scala.util.control.NonFatal
+
 object ApiExceptionHandler extends Directives with LazyLogging {
   val handler: ExceptionHandler = ExceptionHandler {
     case ex : IllegalRestCallArgumentsException =>
       logger.debug(s"Bad request resulted in exception ${ex.getMessage}")
       complete(HttpResponse(BadRequest, entity = ex.getMessage))
-    case ex : Throwable =>
+    case NonFatal(ex) =>
       logger.error(s"Internal server error with exception $ex")
       complete(HttpResponse(InternalServerError, entity = ex.getMessage))
   }
